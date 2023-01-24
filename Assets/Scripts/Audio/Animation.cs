808e84d2486fd5aab75a20aa532039b27c4190ec
Jean-Michel <jean-michel.giverne@edu.devinci.fr>
2023-01-24 15:58:00 +0100

    [Add]Camera Shake

--- Assets/Scripts/Audio/Animation.cs
+++ Assets/Scripts/Audio/Animation.cs
@@ -3,28 +3,45 @@ namespace TeamFive
     using System;
     using UnityEngine;
     using DG.Tweening;
+    using Microsoft.Unity.VisualStudio.Editor;
 
     public class Animation : MonoBehaviour
     {
+
+        public GameObject backGround;
+        public GameObject perso;
+
+        public Image imageLeft;
+        public Image imageMiddle;
+        public Image imageRight;
         private void Update()
         {
             if(Input.GetKeyDown(KeyCode.P))
             {
-
+                CameraShake();
             }
-
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                FadeIN();
+            }
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                FadeOut();
+            }
+            
         }
 
         void CameraShake()
         {
-
+            backGround.transform.DOShakePosition(5f, 8f, 10, 2f, true);
+            perso.transform.DOShakePosition(3f, 8f, 10, 2f, true);
         }
 
         
 
         void FadeIN()
         {
-
+            
         }
 
         void FadeOut()
